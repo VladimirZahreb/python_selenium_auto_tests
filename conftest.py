2e6fb6879d278eb0564a881c43444cb64128710b
VladimirZVV <big09@ukr.net>
2022-12-13 18:08:29 +0200

    Added base_page.py conftest.py

---
+++ conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    # driver = webdriver.Chrome()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
+
+    # service = driver_service
+    # scope = 'function'
